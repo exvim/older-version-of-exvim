c9e31b873f989952141ee4adbaa3079f1548eccb
jwu <jwu@4787614b-4f5a-7849-8442-eb216bebc7ce>
2007-11-02 05:07:32 +0000

    add python support

--- Vim/toolkit/quickgen/quick_gen_project.bat
+++ Vim/toolkit/quickgen/quick_gen_project.bat
@@ -8,6 +8,7 @@ set FILEFILTER="*.c *.cpp *.cxx *.h *.hpp *.inl *.uc *.hlsl *.vsh *.psh *.fx *.f
 if /I "%1"=="all" goto START
 if /I "%1"=="c" goto C_ONLY
 if /I "%1"=="cpp" goto CPP_ONLY
+if /I "%1"=="python" goto PYTHON
 if /I "%1"=="general" goto GENERAL
 if /I "%1"=="vim" goto VIM
 
@@ -18,13 +19,19 @@ rem =======================================
 rem c-only settings
 :C_ONLY
 set LANGTYPE=C_ONLY
-set FILEFILTER="*.c *.h" 
+set FILEFILTER=*.c *.h
 goto START
 
 rem cpp-only settings
 :CPP_ONLY
 set LANGTYPE=CPP_ONLY
-set FILEFILTER="*.cpp *.cxx *.h *.hpp *.inl" 
+set FILEFILTER=*.cpp *.cxx *.h *.hpp *.inl
+goto START
+
+rem python settings
+:PYTHON
+set LANGTYPE=PYTHON
+set FILEFILTER=*.py
 goto START
 
 rem cstyle settings
@@ -36,7 +43,13 @@ goto START
 rem vim settings
 :VIM
 set LANGTYPE=VIM
-set FILEFILTER="*.vim" 
+set FILEFILTER=*.vim
+goto START
+
+rem all settings
+:VIM
+set LANGTYPE=ALL
+set FILEFILTER=*.*
 goto START
 
 rem =======================================
@@ -71,9 +84,10 @@ rem =======================================
 rem create tags
 rem =======================================
 echo Creating Tags...
-if /I "%LANGTYPE%"=="C_ONLY" ctags -o./_tags -R --c++-kinds=+p --fields=+iaS --extra=+q --languages=c --langmap=c++:+.inl
+if /I "%LANGTYPE%"=="C_ONLY" ctags -o./_tags -R --c-kinds=+p --fields=+iaS --extra=+q --languages=c --langmap=c++:+.inl
 if /I "%LANGTYPE%"=="CPP_ONLY" ctags -o./_tags -R --c++-kinds=+p --fields=+iaS --extra=+q --languages=c++ --langmap=c++:+.inl
-if /I "%LANGTYPE%"=="GENERAL" ctags -o./_tags -R --c++-kinds=+p --fields=+iaS --extra=+q --languages=c,c++ --langmap=c++:+.inl
+if /I "%LANGTYPE%"=="PYTHON" ctags -o./_tags -R --fields=+iaS --extra=+q --languages=python
+if /I "%LANGTYPE%"=="GENERAL" ctags -o./_tags -R --c++-kinds=+p --fields=+iaS --extra=+q --languages=c,c++,python --langmap=c++:+.inl
 if /I "%LANGTYPE%"=="VIM" ctags -o./_tags -R  --fields=+iaS --extra=+q --languages=vim
 if /I "%LANGTYPE%"=="ALL" ctags -o./_tags -R --c++-kinds=+p --fields=+iaS --extra=+q
 move /Y ".\_tags" ".\tags"
@@ -94,6 +108,7 @@ rem create inherits
 rem =======================================
 if /I "%LANGTYPE%"=="VIM" goto CSCOPE
 if /I "%LANGTYPE%"=="C_ONLY" goto CSCOPE
+if /I "%LANGTYPE%"=="PYTHON" goto CSCOPE
 echo Creating Inherits...
 gawk -f "%EX_DEV%\Vim\toolkit\gawk\prg_Inherits.awk" ./tags>./_vimfiles/_inherits
 move /Y ".\_vimfiles\_inherits" ".\_vimfiles\inherits"
@@ -104,6 +119,7 @@ rem =======================================
 rem create cscope files but if LANGTYPE is vim, we don't needt to create cscope
 rem =======================================
 if /I "%LANGTYPE%"=="VIM" goto ID
+if /I "%LANGTYPE%"=="PYTHON" goto ID
 echo Creating cscope.files...
 dir /s /b %FILEFILTER%|sed "s,\(.*\),\"\1\",g" > cscope.files
 echo Creating cscope.out...
---
+++ Vim/toolkit/quickgen/quick_gen_project_python_only.bat
@@ -0,0 +1,1 @@
+"%EX_DEV%\Vim\toolkit\quickgen\quick_gen_project.bat" python %1
