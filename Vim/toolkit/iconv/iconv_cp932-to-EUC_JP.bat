8bf20e7594da838a73da3418842f7216d28c654d
jwu <jwu@4787614b-4f5a-7849-8442-eb216bebc7ce>
2007-11-27 09:17:34 +0000

    simply the command

--- Vim/toolkit/iconv/iconv_cp932-to-EUC_JP.bat
+++ Vim/toolkit/iconv/iconv_cp932-to-EUC_JP.bat
@@ -2,8 +2,7 @@ echo off
 for /R %1 %%i in (*.log *.cue *.txt) do ( 
         echo Converting %%i... 
         iconv -c -f cp932 -t EUC-JP "%%i" > "%%i.cov"
-        copy "%%i.cov" "%%i"
-        del "%%i.cov" )
+        move "%%i.cov" "%%i" )
 goto Finish
 :Finish
 echo on
--- Vim/toolkit/iconv/iconv_latin1-to-utf-8.bat
+++ Vim/toolkit/iconv/iconv_latin1-to-utf-8.bat
@@ -2,8 +2,7 @@ echo off
 for /R %1 %%i in (*.c *.cpp *.cxx *.c++ *.C *.h *.H *.hh *.hxx *.hpp *.inl *.uc *.hlsl *.vsh *.psh *.fx *.fxh *.cg *.shd *.txt) do ( 
         echo Converting %%i... 
         iconv -c -f latin1 -t utf-8 "%%i" > "%%i.cov"
-        copy "%%i.cov" "%%i"
-        del "%%i.cov" )
+        copy "%%i.cov" "%%i" )
 goto Finish
 :Finish
 echo on
