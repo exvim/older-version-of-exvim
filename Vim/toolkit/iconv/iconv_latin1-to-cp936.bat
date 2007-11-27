5c62a19b910788dad5ce9f9d64044ac8bb3c9657
jwu <jwu@4787614b-4f5a-7849-8442-eb216bebc7ce>
2007-11-27 09:06:05 +0000

    update iconv to support more files

--- Vim/toolkit/iconv/iconv_latin1-to-cp936.bat
+++ Vim/toolkit/iconv/iconv_latin1-to-cp936.bat
@@ -1,5 +1,5 @@
 echo off
-for /R %1 %%i in (*.c *.cpp *.h *.txt) do ( 
+for /R %1 %%i in (*.c *.cpp *.cxx *.c++ *.C *.h *.H *.hh *.hxx *.hpp *.inl *.uc *.hlsl *.vsh *.psh *.fx *.fxh *.cg *.shd *.txt) do ( 
         echo Converting %%i... 
         iconv -c -f latin1 -t cp936 "%%i" > "%%i.cov"
         copy "%%i.cov" "%%i"
