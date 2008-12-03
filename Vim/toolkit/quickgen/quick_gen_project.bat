35e8748c44242c67e6821d9b8b56f41f3c62e403
jwu <jwu@4787614b-4f5a-7849-8442-eb216bebc7ce>
2008-12-03 03:16:33 +0000

    add ctags support math and uc

--- Vim/toolkit/quickgen/quick_gen_project.bat
+++ Vim/toolkit/quickgen/quick_gen_project.bat
@@ -12,21 +12,32 @@ rem preprocess arguments
 rem /////////////////////////////////////////////////////////////////////////////
 
 set LANGTYPE=ALL
-set FILEFILTER="*.c *.cpp *.cxx *.h *.hpp *.inl *.uc *.hlsl *.vsh *.psh *.fx *.fxh *.cg *.shd"
+set FILEFILTER="*.c *.cpp *.cxx *.h *.hpp *.inl *.hlsl *.vsh *.psh *.fx *.fxh *.cg *.shd *.uc *.m"
 if /I "%1"=="all" goto START
+if /I "%1"=="general" goto GENERAL
 if /I "%1"=="c" goto C_ONLY
 if /I "%1"=="cpp" goto CPP_ONLY
-if /I "%1"=="python" goto PYTHON
 if /I "%1"=="c#" goto CSHARP
-if /I "%1"=="general" goto GENERAL
-if /I "%1"=="vim" goto VIM
-if /I "%1"=="lua" goto LUA
-if /I "%1"=="js" goto JS
 if /I "%1"=="html" goto HTML
+if /I "%1"=="js" goto JS
+if /I "%1"=="lua" goto LUA
+if /I "%1"=="math" goto MATH
+if /I "%1"=="python" goto PYTHON
+if /I "%1"=="uc" goto UC
+if /I "%1"=="vim" goto VIM
 
 rem /////////////////////////////////////////////////////////////////////////////
 rem set variables
 rem /////////////////////////////////////////////////////////////////////////////
+
+rem  ------------------------------------------------------------------ 
+rem  Desc: cstyle settings
+rem  ------------------------------------------------------------------ 
+
+:GENERAL
+set LANGTYPE=GENERAL
+set FILEFILTER=*.c *.cpp *.cxx *.h *.hpp *.inl *.hlsl *.vsh *.psh *.fx *.fxh *.cg *.shd *.uc *.m
+goto START
 
 rem  ------------------------------------------------------------------ 
 rem  Desc: c-only settings
@@ -47,30 +58,12 @@ set FILEFILTER=*.cpp *.cxx *.h *.hpp *.inl
 goto START
 
 rem  ------------------------------------------------------------------ 
-rem  Desc: python settings
-rem  ------------------------------------------------------------------ 
-
-:PYTHON
-set LANGTYPE=PYTHON
-set FILEFILTER=*.py
-goto START
-
-rem  ------------------------------------------------------------------ 
 rem  Desc: c# settings
 rem  ------------------------------------------------------------------ 
 
 :CSHARP
 set LANGTYPE=CSHARP
 set FILEFILTER=*.cs
-goto START
-
-rem  ------------------------------------------------------------------ 
-rem  Desc: vim settings
-rem  ------------------------------------------------------------------ 
-
-:VIM
-set LANGTYPE=VIM
-set FILEFILTER=*.vim
 goto START
 
 rem  ------------------------------------------------------------------ 
@@ -83,15 +76,6 @@ set FILEFILTER=*.html *.htm *.shtml *.stm
 goto START
 
 rem  ------------------------------------------------------------------ 
-rem  Desc: lua settings
-rem  ------------------------------------------------------------------ 
-
-:LUA
-set LANGTYPE=LUA
-set FILEFILTER=*.lua
-goto START
-
-rem  ------------------------------------------------------------------ 
 rem  Desc: javascript settings
 rem  ------------------------------------------------------------------ 
 
@@ -101,12 +85,48 @@ set FILEFILTER=*.js *.as
 goto START
 
 rem  ------------------------------------------------------------------ 
-rem  Desc: cstyle settings
+rem  Desc: lua settings
 rem  ------------------------------------------------------------------ 
 
-:GENERAL
-set LANGTYPE=GENERAL
-set FILEFILTER=*.c *.cpp *.cxx *.h *.hpp *.inl *.uc *.hlsl *.vsh *.psh *.fx *.fxh *.cg *.shd *.m
+:LUA
+set LANGTYPE=LUA
+set FILEFILTER=*.lua
+goto START
+
+rem  ------------------------------------------------------------------ 
+rem  Desc: 
+rem  ------------------------------------------------------------------ 
+
+:MATH
+set LANGTYPE=MATH
+set FILEFILTER=*.m
+goto START
+
+rem  ------------------------------------------------------------------ 
+rem  Desc: python settings
+rem  ------------------------------------------------------------------ 
+
+:PYTHON
+set LANGTYPE=PYTHON
+set FILEFILTER=*.py
+goto START
+
+rem  ------------------------------------------------------------------ 
+rem  Desc: 
+rem  ------------------------------------------------------------------ 
+
+:UC
+set LANGTYPE=UC
+set FILEFILTER=*.uc
+goto START
+
+rem  ------------------------------------------------------------------ 
+rem  Desc: vim settings
+rem  ------------------------------------------------------------------ 
+
+:VIM
+set LANGTYPE=VIM
+set FILEFILTER=*.vim
 goto START
 
 rem /////////////////////////////////////////////////////////////////////////////
@@ -148,16 +168,18 @@ rem  Desc: create tags
 rem  ------------------------------------------------------------------ 
 
 echo Creating Tags...
+if /I "%LANGTYPE%"=="ALL" ctags -o./_tags -R --c++-kinds=+p --fields=+iaS --extra=+q --languages=c,c++,c#,python,vim,html,lua,javascript,uc,math --langmap=c++:+.inl,c:+.fx,c:+.fxh,c:+.hlsl,c:+.vsh,c:+.psh,c:+.cg,c:+.shd,javascript:+.as
+if /I "%LANGTYPE%"=="GENERAL" ctags -o./_tags -R --c++-kinds=+p --fields=+iaS --extra=+q --languages=c,c++,c#,python --langmap=c++:+.inl,c:+.fx,c:+.fxh,c:+.hlsl,c:+.vsh,c:+.psh,c:+.cg,c:+.shd
 if /I "%LANGTYPE%"=="C_ONLY" ctags -o./_tags -R --c-kinds=+p --fields=+iaS --extra=+q --languages=c --langmap=c++:+.inl
 if /I "%LANGTYPE%"=="CPP_ONLY" ctags -o./_tags -R --c++-kinds=+p --fields=+iaS --extra=+q --languages=c++ --langmap=c++:+.inl
-if /I "%LANGTYPE%"=="PYTHON" ctags -o./_tags -R --fields=+iaS --extra=+q --languages=python
 if /I "%LANGTYPE%"=="CSHARP" ctags -o./_tags -R --fields=+iaS --extra=+q --languages=c#
-if /I "%LANGTYPE%"=="VIM" ctags -o./_tags -R  --fields=+iaS --extra=+q --languages=vim
 if /I "%LANGTYPE%"=="HTML" ctags -o./_tags -R  --fields=+iaS --extra=+q --languages=html
-if /I "%LANGTYPE%"=="LUA" ctags -o./_tags -R  --fields=+iaS --extra=+q --languages=lua
 if /I "%LANGTYPE%"=="JS" ctags -o./_tags -R  --fields=+iaS --extra=+q --languages=javascript --langmap=javascript:+.as
-if /I "%LANGTYPE%"=="GENERAL" ctags -o./_tags -R --c++-kinds=+p --fields=+iaS --extra=+q --languages=c,c++,c#,python --langmap=c++:+.inl,c:+.fx,c:+.fxh,c:+.hlsl,c:+.vsh,c:+.psh,c:+.cg,c:+.shd
-if /I "%LANGTYPE%"=="ALL" ctags -o./_tags -R --c++-kinds=+p --fields=+iaS --extra=+q --languages=c,c++,c#,python,vim,html,lua,javascript --langmap=c++:+.inl,c:+.fx,c:+.fxh,c:+.hlsl,c:+.vsh,c:+.psh,c:+.cg,c:+.shd,javascript:+.as
+if /I "%LANGTYPE%"=="LUA" ctags -o./_tags -R  --fields=+iaS --extra=+q --languages=lua
+if /I "%LANGTYPE%"=="MATH" ctags -o./_tags -R  --fields=+iaS --extra=+q --languages=math
+if /I "%LANGTYPE%"=="PYTHON" ctags -o./_tags -R --fields=+iaS --extra=+q --languages=python
+if /I "%LANGTYPE%"=="UC" ctags -o./_tags -R  --fields=+iaS --extra=+q --languages=uc
+if /I "%LANGTYPE%"=="VIM" ctags -o./_tags -R  --fields=+iaS --extra=+q --languages=vim
 move /Y ".\_tags" ".\tags"
 
 :SYMBOL
@@ -180,9 +202,10 @@ rem  ------------------------------------------------------------------
 
 if /I "%2"=="symbol" goto FINISH
 if /I "%LANGTYPE%"=="C_ONLY" goto CSCOPE
-if /I "%LANGTYPE%"=="VIM" goto CSCOPE
 if /I "%LANGTYPE%"=="HTML" goto CSCOPE
 if /I "%LANGTYPE%"=="LUA" goto CSCOPE
+if /I "%LANGTYPE%"=="MATH" goto CSCOPE
+if /I "%LANGTYPE%"=="VIM" goto CSCOPE
 echo Creating Inherits...
 gawk -f "%EX_DEV%\Vim\toolkit\gawk\prg_Inherits.awk" ./tags>./_vimfiles/_inherits
 move /Y ".\_vimfiles\_inherits" ".\_vimfiles\inherits"
@@ -195,12 +218,14 @@ rem  NOTE: only for c/cpp
 rem  ------------------------------------------------------------------ 
 
 if /I "%2"=="inherits" goto FINISH
-if /I "%LANGTYPE%"=="PYTHON" goto ID
 if /I "%LANGTYPE%"=="CSHARP" goto ID
-if /I "%LANGTYPE%"=="VIM" goto ID
+if /I "%LANGTYPE%"=="PYTHON" goto ID
 if /I "%LANGTYPE%"=="HTML" goto ID
-if /I "%LANGTYPE%"=="LUA" goto ID
 if /I "%LANGTYPE%"=="JS" goto ID
+if /I "%LANGTYPE%"=="LUA" goto ID
+if /I "%LANGTYPE%"=="MATH" goto ID
+if /I "%LANGTYPE%"=="UC" goto ID
+if /I "%LANGTYPE%"=="VIM" goto ID
 echo Creating cscope.files...
 dir /s /b %FILEFILTER%|sed "s,\(.*\),\"\1\",g" > cscope.files
 echo Creating cscope.out...
---
+++ Vim/toolkit/quickgen/quick_gen_project_math_only.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem  ======================================================================================
+rem  File         : quick_gen_project_vim_only.bat
+rem  Author       : Wu Jie 
+rem  Last Change  : 10/19/2008 | 15:32:10 PM | Sunday,October
+rem  Description  : 
+rem  ======================================================================================
+
+rem  ------------------------------------------------------------------ 
+rem  Desc: 
+rem  ------------------------------------------------------------------ 
+
+"%EX_DEV%\Vim\toolkit\quickgen\quick_gen_project.bat" math %1
+
+echo on
